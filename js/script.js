fc41248f1f78341a5b9c8a82be78030d346825da
chaaeyoung <151603646+chaaeyoung@users.noreply.github.com>
2024-01-23 14:22:06 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(function () {
         /* 1 */
         $(window).scroll(function () {
             /* 2 */
-            $('.Ptext1, .Ptext2, .Ptext3, .Ptext4, .Ptext5, .minLogo').each(function (i) {
+            $('.Ptext1, .Ptext2, .Ptext3, .Ptext4, .Ptext5, .minLogo, .InDbtn>a').each(function (i) {
                 var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 5;
                 var bottom_of_window = $(window).scrollTop() + $(window).height(); /* 3 */
                 if (bottom_of_window > bottom_of_element) {
