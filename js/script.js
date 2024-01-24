295353fa3243b382c8100a149b611b6ee50a6daa
chaaeyoung <151603646+chaaeyoung@users.noreply.github.com>
2024-01-24 13:22:11 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,5 +15,44 @@ $(function () {
             });
         });
     });
+        
+        $('.searchLang_l > li').on('mouseover', function(){
+            $('.sub-wrap').stop().slideUp(200)
+            $(this).children('.sub-wrap').stop().slideDown(200)
+        });
+        
+        $('.searchLang_l > li').on('mouseleave', function(){
+            $(this).children('.sub-wrap').stop().slideUp(200)
+        });
 
-});
+        $(function(){
+ 
+            $(".toggle").click(function()    {
+              if($(".toggle").hasClass('on')){
+                $(".toggle").removeClass('on');
+                $("#slide").removeClass('on');
+               
+              } else{
+                $("#toggle").addClass('on');
+               $("#slide").addClass('on'); 
+              }
+            });
+            
+          });
+
+    // $(".toggle").on("click", function () {
+    //     $(".bg").fadeIn();
+    // });
+    // $(".toggle").on("click", function () {
+    //     $(".bg").fadeOut();
+    // });
+
+    $('.toggel').click(function(){
+        $(this).addClass('.bg')
+    });
+
+    $('.toggle').click(function(){
+        $('this').removeClass('.bg')
+    });
+
+    });
