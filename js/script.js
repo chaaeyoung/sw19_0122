9529ebd7f65b88527b09f49134e10c1ddef55b3f
chaaeyoung <151603646+chaaeyoung@users.noreply.github.com>
2024-01-24 17:57:42 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -16,6 +16,8 @@ $(function () {
         });
     });
         
+
+
         $('.searchLang_l > li').on('mouseover', function(){
             $('.sub-wrap').stop().slideUp(200)
             $(this).children('.sub-wrap').stop().slideDown(200)
@@ -25,34 +27,38 @@ $(function () {
             $(this).children('.sub-wrap').stop().slideUp(200)
         });
 
-        $(function(){
- 
-            $(".toggle").click(function()    {
-              if($(".toggle").hasClass('on')){
-                $(".toggle").removeClass('on');
-                $("#slide").removeClass('on');
-               
-              } else{
-                $("#toggle").addClass('on');
-               $("#slide").addClass('on'); 
-              }
+
+
+            $(".toggle").click(function (event) {
+                event.stopPropagation(); // 슬라이드 토글 버튼 클릭 시, 이벤트 전파 중지
+                if ($("#toggle").hasClass('on')) {
+                    $(".toggle").removeClass('on');
+                    $("#slide").removeClass('on');
+                    $(".bg").removeClass('overlay'); // 슬라이드가 닫힐 때 overlay 클래스 제거
+                    // $(".Xbtn").hide();
+                } else {
+                    $("#toggle").addClass('on');
+                    $("#slide").addClass('on');
+                    $(".bg").addClass('overlay'); // 슬라이드가 열릴 때 overlay 클래스 추가
+                    // $(".Xbtn").show();
+                }
             });
+        
+            // document에 클릭 이벤트 추가
+            $(document).click(function (event) {
+                // 클릭된 요소가 슬라이드 내부인지 확인
+                // 이벤트 타겟이 #slide 요소의 자손이 아닌 경우를 확인. #slide 요소의 자손이 아니면 참이 됨.
+                // !$(event.target).hasClass('toggle'): 이 부분은 이벤트 타겟이 toggle 클래스를 가진 요소가 아닌 경우를 확인
+                if (!$(event.target).closest('#slide').length && !$(event.target).hasClass('toggle')) {
+                    // 슬라이드 닫기
+                    $(".toggle").removeClass('on');
+                    $("#slide").removeClass('on');
+                    $(".bg").removeClass('overlay'); // 슬라이드가 닫힐 때 overlay 클래스 제거
+                }
+            });
+
             
-          });
-
-    // $(".toggle").on("click", function () {
-    //     $(".bg").fadeIn();
-    // });
-    // $(".toggle").on("click", function () {
-    //     $(".bg").fadeOut();
-    // });
-
-    $('.toggel').click(function(){
-        $(this).addClass('.bg')
+        $(".toggle").on("click", function () {
+        $(".bg").fadeIn();
     });
-
-    $('.toggle').click(function(){
-        $('this').removeClass('.bg')
-    });
-
     });
